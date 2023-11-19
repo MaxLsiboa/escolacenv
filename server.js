b3a2f518cfe2c68544c5e5510eed11c135e74377
MaxLsiboa <131786808+MaxLsiboa@users.noreply.github.com>
2023-11-19 20:53:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/admin', authenticateUser, (req, res) => {
   if (req.session.user && req.session.user.role === 'admin') {
     res.sendFile(__dirname + '/public/views/admin.html');
   } else {
-    res.redirect('/');
+    res.redirect('/public/scripts/admin.html');
   }
 });
 
@@ -105,4 +105,69 @@ app.get('/logout', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+});
+// Rota para adicionar um aluno (requer autenticação de administrador)
+app.post('/addAluno', authenticateUser, (req, res) => {
+  // Verifique se o usuário autenticado é um administrador
+  if (req.session.user && req.session.user.role === 'admin') {
+    const { nome, matricula, curso } = req.body;
+
+    // Adicione lógica para inserir o aluno no banco de dados
+    connection.query(
+      'INSERT INTO alunos (nome, matricula, curso) VALUES (?, ?, ?)',
+      [nome, matricula, curso],
+      (err, results) => {
+        if (err) {
+          console.error('Erro ao adicionar aluno:', err);
+          res.redirect('/admin');
+        } else {
+          res.redirect('/admin');
+        }
+      }
+    );
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Rota para visualizar todos os alunos (requer autenticação de administrador)
+app.get('/viewAlunos', authenticateUser, (req, res) => {
+  // Verifique se o usuário autenticado é um administrador
+  if (req.session.user && req.session.user.role === 'admin') {
+    // Adicione lógica para buscar todos os alunos no banco de dados
+    connection.query('SELECT * FROM alunos', (err, results) => {
+      if (err) {
+        console.error('Erro ao buscar alunos:', err);
+        res.redirect('/admin');
+      } else {
+        res.render('viewAlunos', { alunos: results });
+      }
+    });
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Rota para remover um aluno (requer autenticação de administrador)
+app.post('/removeAluno', authenticateUser, (req, res) => {
+  // Verifique se o usuário autenticado é um administrador
+  if (req.session.user && req.session.user.role === 'admin') {
+    const { alunoId } = req.body;
+
+    // Adicione lógica para remover o aluno do banco de dados
+    connection.query(
+      'DELETE FROM alunos WHERE id = ?',
+      [alunoId],
+      (err, results) => {
+        if (err) {
+          console.error('Erro ao remover aluno:', err);
+          res.redirect('/admin');
+        } else {
+          res.redirect('/admin');
+        }
+      }
+    );
+  } else {
+    res.redirect('/');
+  }
 });
